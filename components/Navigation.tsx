8f9f67f3000ea8a586a0853bf4c3a6c831597a1b
Amon Vanderlei <amon.chalegre@hotmail.com>
2025-10-11 21:54:06 -0300

    First commit

---
+++ components/Navigation.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";   
+
+export default function Navigation() {
+  return (
+    <nav className="bg-cyan-700 w-full h-20 flex justify-between items-center p-8">
+      <Link href="/">AXIOMA</Link>
+      <div className="flex gap-4 justify-between">
+        <a href="https://impatech.edu.br/institucional/conheca-o-impa-tech/">
+          Sobre o IMPA Tech
+        </a>
+        <Link href="/sobre">Sobre o Axioma</Link>
+        <Link href="/regulamento">Regulamento</Link>
+        <Link href="/contato">Contato</Link>
+        <Link href="inscricao">Inscreva-se</Link>
+      </div>
+    </nav>
+  );
+}
